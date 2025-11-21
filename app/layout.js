cc75310eb211b5855d7e9cd32eb81c09ee2d4b15
sidhant kumar <23f1001556@ds.study.iitm.ac.in>
2025-11-21 12:40:36 +0530

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,28 @@
-import {Outfit,Ovo } from "next/font/google";
+import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
-const outfit= Outfit({
-  subsets: ["latin"],weight:["400","500","600","700"],
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
-const ovo= Ovo({
-  subsets: ["latin"],weight:["400"],
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"],
 });
+
 export const metadata = {
   title: "Portfolio App",
-  description: "",
+  description: "Personal portfolio of Sidhant Kumar showcasing projects and skills.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='scroll-smooth'>
-      <body
-        className={`${outfit.className} ${ovo.className} antialiased`}
-      >
+    <html lang="en" className="scroll-smooth">
+      <body className={`${outfit.className} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
